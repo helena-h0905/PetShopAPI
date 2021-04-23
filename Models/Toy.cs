19dc1a2e2d63308874dc9c4766a9b55f25226f73
helena-h0905 <31130501+helena-h0905@users.noreply.github.com>
2021-04-23 10:54:51 +0200

    Update Toy.cs

--- Models/Toy.cs
+++ Models/Toy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 #nullable disable
@@ -11,7 +11,7 @@ namespace PetShopAPI.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int? AnimalId { get; set; }
-        public decimal? Toy { get; set; }
+        public decimal? Price { get; set; }
 
         public virtual Animal Animal { get; set; }
     }
